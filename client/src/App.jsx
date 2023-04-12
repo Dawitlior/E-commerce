7ec174b14a89dd3ce56c438cbf2ffe318ca30d3a
Dawitlior <dawitlior777@gmail.com>
2023-04-12 19:26:27 +0300

    routes configuration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,11 @@
 import { useEffect } from "react";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import Home from "@/scenes/home/home";
+import ItemDetails from "@/scenes/itemDetails/itemDetails";
+import Checkout from "@/scenes/checkout/Checkout";
+import Confirmation from "@/scenes/checkout/Confirmation";
+import Navbar from "@/scenes/global/Navbar";
+import CartMenu from "@/scenes/global/CartMenu";
 
 const App = () => {
   const ScrollToTop = () => {
@@ -12,7 +18,21 @@ const App = () => {
     return null;
   };
 
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+      <Navbar />
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="item/:itemId" element={<ItemDetails />} />
+          <Route path="checkout" element={<Checkout />} />
+          <Route path="checkout/success" element={<Confirmation />} />
+        </Routes>
+        <CartMenu />
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
