2a12d2ae0701135a2c678543f62d46973da68cc0
Dawitlior <dawitlior777@gmail.com>
2023-04-13 22:52:57 +0300

    q

--- client/src/scenes/checkout/Checkout.jsx
+++ client/src/scenes/checkout/Checkout.jsx
@@ -3,7 +3,7 @@ import { Box, Button, Stepper, Step, StepLabel } from "@mui/material";
 import { Formik } from "formik";
 import { useState } from "react";
 import * as yup from "yup";
-import { shades } from "../../theme";
+import { shades } from "@/theme";
 // import Payment from "./Payment";
 // import Shipping from "./Shipping";
 // import { loadStripe } from "@stripe/stripe-js";
