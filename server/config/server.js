ead8d04a4effbe689055862e264cd9e0b74b6325
Dawitlior <dawitlior777@gmail.com>
2023-04-14 18:16:07 +0300

    add

--- server/config/server.js
+++ server/config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: "https://server-ecommerce.netlify.app",
   app: {
     keys: env.array('APP_KEYS'),
   },
