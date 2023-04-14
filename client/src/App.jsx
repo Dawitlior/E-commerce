f8693bceee4bf64ebd6854e47d3fbab2740a5b30
Dawitlior <dawitlior777@gmail.com>
2023-04-14 17:27:46 +0300

    add

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
-import Home from "@/scenes/home/home";
+import Home from "@/scenes/home/Home";
 import ItemDetails from "@/scenes/itemDetails/itemDetails";
 import Checkout from "@/scenes/checkout/Checkout";
 import Confirmation from "@/scenes/checkout/Confirmation";
